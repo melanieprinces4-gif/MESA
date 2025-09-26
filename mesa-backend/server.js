3c255aba738bfa22bb84ab0373a7d6b4da5f0d83
alex <sierraa8298@student.laccd.edu>
2025-09-26 14:31:29 -0700

    Update server.js

--- mesa-backend/server.js
+++ mesa-backend/server.js
@@ -10,27 +10,26 @@ app.use(cors({ origin: '*' }));
 
 // Sample data: array of student objects
 const students = [
-  { name: "Alice", funFacts: ["Loves painting", "Has a cat", "Speaks 3 languages"] },
-  { name: "Bob", funFacts: ["Plays guitar", "Enjoys hiking", "Collects stamps"] },
-  { name: "Charlie", funFacts: ["Chess champion", "Enjoys cooking", "Plays soccer"] },
-  { name: "Diana", funFacts: ["Loves reading", "Has a dog", "Knows karate"] },
-  { name: "Ethan", funFacts: ["Enjoys coding", "Plays piano", "Likes cycling"] },
-  { name: "Fiona", funFacts: ["Likes photography", "Speaks French", "Gardens"] },
-  { name: "George", funFacts: ["Swims competitively", "Plays violin", "Enjoys gaming"] },
-  { name: "Hannah", funFacts: ["Writes poetry", "Loves sushi", "Has a hamster"] },
-  { name: "Ian", funFacts: ["Plays basketball", "Enjoys travel", "Speaks Spanish"] },
-  { name: "Julia", funFacts: ["Loves yoga", "Likes coffee", "Has a parrot"] },
-  { name: "Kevin", funFacts: ["Enjoys skateboarding", "Plays drums", "Collects coins"] },
-  { name: "Laura", funFacts: ["Loves baking", "Speaks Italian", "Enjoys painting"] },
-  { name: "Michael", funFacts: ["Plays tennis", "Likes photography", "Has a lizard"] },
-  { name: "Nina", funFacts: ["Enjoys running", "Loves movies", "Speaks German"] },
-  { name: "Oscar", funFacts: ["Plays football", "Likes chess", "Has a cat"] },
-  { name: "Paula", funFacts: ["Enjoys singing", "Likes hiking", "Loves tea"] },
-  { name: "Quentin", funFacts: ["Plays piano", "Enjoys coding", "Collects postcards"] },
-  { name: "Rachel", funFacts: ["Loves knitting", "Speaks Japanese", "Has a dog"] },
-  { name: "Sam", funFacts: ["Plays guitar", "Likes swimming", "Enjoys reading"] },
-  { name: "Tina", funFacts: ["Enjoys photography", "Loves baking", "Has a rabbit"] },
-];
+  { name: "Jazieh", funFacts: ["Graduate with 5 AA degrees ", "Have twins ", "I went to Tokyo "] },
+  { name: "Grecia", funFacts: ["Plays guitar", "Enjoys hiking", "Enjoy baking"] },
+  { name: "Alexander", funFacts: ["Chess champion", "Enjoys cooking", "Plays soccer"] },
+  { name: "Jeffrey", funFacts: ["Loves reading", "Has a dog", "Knows karate"] },
+  { name: "Jazieh", funFacts: ["Enjoys coding", "Plays piano", "Likes cycling"] },
+  { name: "Phuthachath", funFacts: ["There’s 13 letters in my name", "My middle name is longer", "I have cat"] },
+  { name: "Ken", funFacts: ["Likes photography", "Speaks French", "Gardens"] },
+  { name: "Lyzander Buendia", funFacts: ["Likes photography", "Speaks French", "Gardens"] },
+  { name: "kamal", funFacts: ["Swims competitively", "Plays violin", "Enjoys gaming"] },
+  { name: "Ken", funFacts: ["Writes poetry", "Loves sushi", "Has a hamster"] },
+  { name: "Melanie Rosas", funFacts: ["I have 1 brother", "I own a chameleon", "I love tomato's"] },
+  { name: "Cecilia Herrera", funFacts: ["I have an identical twin ", "I can play the guitar ", "I can’t swim"] },
+  { name: "Junior", funFacts: ["I have a dog", "I have 16 chickens", "I’ve never broken/fractured a bone"] },
+  { name: "Isabella ", funFacts: ["I have a leopard gecko ", "I work at a small coffee ", "My middle name is Maya"] },
+  { name: "Edith Aguilar", funFacts: ["Favorite color is purple", "Favorite animal are cats", "I have a pet rabbit"] },
+  { name: "Gabriel Roman", funFacts: ["I like bananas", "I like pineapples", "I like oranges"] },
+  { name: "Jamila ramzyar", funFacts: ["I love being out siding ", "I love my family ", "I can’t stand people "] },
+  { name: "Luis", funFacts: ["I like watching anime ", "I love transformers ", "I started watching the Mobile Suit Gundam shows"] },
+]
+  
 // Route to return the entire array
 app.get('/api/students', (req, res) => {
   res.json(students);
